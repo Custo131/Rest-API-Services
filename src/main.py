70dec339c75516f587c47c63a5c437ecbcca7108
Viktoriia Safiullina <viktoriia.safiullina@rossum.ai>
2025-03-11 14:28:25 +0100

    Add dependency for db, root endpoint

--- src/main.py
+++ src/main.py
@@ -1,20 +1,11 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, Depends, Response
+from http import HTTPStatus
+from database import get_db
 
 
 app = FastAPI()
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+@app.get('/')
+def root(db=Depends(get_db)):
+    return Response(status_code=HTTPStatus.OK)
