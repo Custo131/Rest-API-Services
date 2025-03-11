6ed5c36be1b5f8e2015890aa65fb54964cfa2288
Kirill Tarasov <cyril.tarasov@gmail.com>
2025-03-11 12:58:37 +0100

    removing uneccesary libraries and code from main.py

--- src/main.py
+++ src/main.py
@@ -1,20 +1,10 @@
-from fastapi import FastAPI, Depends
-from sqlalchemy.orm import Session
-from database import SessionLocal, User, UserCreate
+from fastapi import FastAPI
+
+
 
 app = FastAPI()
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
 
-
-@app.get("/")
-def read_root():
-    return {"message": "Hello World"}
 
 
 
