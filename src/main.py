83d499f8e4110397678f38615f8de1c6ad1a25f7
Kirill Tarasov <cyril.tarasov@gmail.com>
2025-03-12 14:55:57 +0100

    addding routes method for testing

--- src/main.py
+++ src/main.py
@@ -1,9 +1,15 @@
 from fastapi import FastAPI, Depends, Response
 from http import HTTPStatus
 from src.database import get_db
+from routes import router 
+
+
 
 
 app = FastAPI()
+
+app.include_router(router)
+
 
 
 @app.get('/')
