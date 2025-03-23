c09b69448140b08db25f62e6d870245aff23e291
Kirill Tarasov <cyril.tarasov@gmail.com>
2025-03-23 21:31:39 +0100

    adding algorithm, secret key and expiration for JWT

--- src/config.py
+++ src/config.py
@@ -2,4 +2,15 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+
 DATABASE_URL = os.environ.get("DATABASE_URL", "")
+SECRET_KEY = os.environ.get("SECRET_KEY")
+ALGORITHM = os.environ.get("ALGORITHM")
+ACCESS_TOKEN_EXPIRE_MINUTES = int(os.environ.get("ACCESS_TOKEN_EXPIRE_MINUTES", 30))
+
+
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL is not set.")
+
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY is not set.")
